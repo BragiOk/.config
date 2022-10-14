fb37e5e705e7012dfdc8a1ce178fa6627ab67ded
brgi <bragi@mail.com>
2022-10-14 05:12:33 +0300

    f

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash bin
 cd
-wget https://raw.githubusercontent.com/BragiOk/.tmux/main/.tmux.conf
-wget https://raw.githubusercontent.com/BragiOk/.tmux/main/.tmux.conf.local
+wget -O https://raw.githubusercontent.com/BragiOk/.tmux/main/.tmux.conf
+wget -O https://raw.githubusercontent.com/BragiOk/.tmux/main/.tmux.conf.local
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 alias tmux='tmux -u'
